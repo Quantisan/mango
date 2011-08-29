a71d3075fb632833e3633dbb76991b2d12ee76da
Paul Lam <paul@quantisan.com>
2011-08-29 18:12:42 -0400

    time series coercion via clj-time

--- src/mango/core.clj
+++ src/mango/core.clj
@@ -1,1 +1,16 @@
-(ns mango.core)
+(ns mango.core
+  (:require [clj-time.core :as time]
+            [clj-time.coerce :as time.coerce])  
+  (:use [incanter core mongodb]
+        [somnium.congomongo]))
+
+;; credit to: Xavier Shay
+(defn transform-column
+" Apply function f to the specified column of data and replace the column
+  with new values."
+  [column f data] 
+  (let [new-col-names (sort-by (partial = column) (col-names data))
+        new-dataset (conj-cols
+                      (sel data :except-cols column)
+                      ($map f column data))]
+    ($ (col-names data) (col-names new-dataset new-col-names))))
--- test/mango/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns mango.test.core
-  (:use [mango.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
